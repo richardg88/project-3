355b57311e4477907e9eeda84087a9bffd99af18
richardg88 <richard_grosvenor@hotmail.com>
2022-07-14 12:17:19 +0000

    Add break to break out of while loop for guessing numbers

--- run.py
+++ run.py
@@ -1,4 +1,32 @@
 import random
 
-r = random.randint(-5, 11)
-print(r)
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type a number larger than o next time.')
+        quit()
+else:
+    print('Please type a number')
+    quit()        
+
+random_number = random.randint(0, top_of_range)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time')
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+    else:
+        print("You got it wrong!")                
+
+
+
+
