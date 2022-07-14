4d47896b9ca3452243947d016fe22fc5dba723be
richardg88 <richard_grosvenor@hotmail.com>
2022-07-14 12:48:39 +0000

    Add commenting to explain code sections

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import random
 
 top_of_range = input("Type a number: ")
-
+"""
+Sets out the number to be added to create the range for guesses
+"""
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
@@ -13,8 +15,13 @@ else:
     quit()        
 
 random_number = random.randint(0, top_of_range)
-
+guesses = 0
+"""
+This section allows the user to make a guess what they think the number is 
+and prevents and words or letters being guessed, only numbers
+"""
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -24,8 +31,18 @@ while True:
 
     if user_guess == random_number:
         print("You got it!")
+        break
     else:
-        print("You got it wrong!")                
+        if user_guess > random_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number")
+
+"""        
+This section tells the user how many times it took them to guess the 
+correct answer
+"""
+print("You got it right in", guesses, "guesses")               
 
 
 
